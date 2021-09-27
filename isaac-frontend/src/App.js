a0817031609ccd53104d1162b2f3b5ddfc6b4e46
filip.andrei <450402@student.fontys.nl>
2021-09-27 11:52:11 +0200

    fetch data from json-server

--- isaac-frontend/src/App.js
+++ isaac-frontend/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 import React from 'react';
+import {useEffect} from 'react';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:5000/entries');
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       s
