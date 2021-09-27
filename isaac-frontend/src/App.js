836331dd776cd31cc33532109f21d80d10d9cd47
filip.andrei <450402@student.fontys.nl>
2021-09-27 11:03:41 +0200

    adde mock data

--- isaac-frontend/src/App.js
+++ isaac-frontend/src/App.js
@@ -1,10 +1,10 @@
+import './App.css';
 import React from 'react';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div>
+      s
     </div>
   );
 }
