0091e8115f2db06cda10462d9e9993fa53385e3b
filip.andrei <450402@student.fontys.nl>
2021-10-04 22:35:04 +0200

    updated mock data in db.json display the average for each day of the current week and last week added empty serverFecth.js to be filled tomorrow

--- isaac-frontend/src/components/DashboardGraphs.js
+++ isaac-frontend/src/components/DashboardGraphs.js
@@ -1,15 +1,25 @@
-import React, {useState, useEffect} from 'react';
 import LineGraph from './LineGraph';
+import React, {useState, useEffect} from 'react';
 import {startOfWeek, endOfWeek} from 'date-fns';
 
 function DashboardGraphs() {
-  //  fetching data from json-server
   const [data, setData] = useState([]);
 
+  //  fetching data from json-server
   useEffect( async () => {
     const res = await fetch('http://localhost:5000/entries');
-    setData(await res.json());
+    const rawData = await res.json();
+    setData(await rawData.map((obj) => {
+      obj.dateTime = new Date(obj.dateTime);
+      return obj;
+    }));
   }, []);
+
+  function getLastWeekDate() {
+    const date = new Date();
+    date.setDate(date.getDate()-7);
+    return date;
+  }
 
   const graphData = {
     labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
@@ -17,8 +27,9 @@ function DashboardGraphs() {
     datasets: [
       {
         fill: true,
-        label: 'Temperature',
-        data: data.map((obj )=> obj.temp),
+        label: 'This week',
+        // filter to return an array with temps of current week
+        data: getTempsArray(new Date()),
         backgroundColor: [
           'rgba(13, 99, 132, 0.3)',
         ],
@@ -29,8 +40,8 @@ function DashboardGraphs() {
       },
       {
         fill: false,
-        label: 'Humidity',
-        data: data.map((obj )=> obj.humidity),
+        label: 'Last week',
+        data: getTempsArray(getLastWeekDate()),
         backgroundColor: [
           'rgba(146, 35, 168, 0.3)',
         ],
@@ -41,10 +52,37 @@ function DashboardGraphs() {
       },
     ],
   };
-  function test() {
-    const firstDay = startOfWeek(new Date(), {weekStartsOn: 1});
-    const lastDay = endOfWeek(new Date(), {weekStartsOn: 1});
-    console.log(firstDay, lastDay);
+  function getTempsArray(date) {
+    const firstDay = startOfWeek(date, {weekStartsOn: 1});
+    const lastDay = endOfWeek(date, {weekStartsOn: 1});
+
+    // get an array with entries of current week
+    const weekEntries = data.filter((obj) =>
+      obj.dateTime >= firstDay && obj.dateTime <= lastDay);
+
+    const average = [0, 0, 0, 0, 0, 0, 0];
+    const counter = [0, 0, 0, 0, 0, 0, 0];
+
+    // used so Monday is first day of the week not Sunday
+    function getDayExtended(date) {
+      let day = date.getDay();
+      if (day == 0) {
+        return 6;
+      }
+      return --day;
+    };
+    // sum the temps for a day and create counter
+    weekEntries.forEach((element) => {
+      const arrayIndex = getDayExtended(element.dateTime);
+      average[arrayIndex] += element.temp;
+      counter[arrayIndex]++;
+    });
+    // calculate average
+    for (let index = 0; index < average.length; index++) {
+      average[index] /= counter[index];
+      average[index] = average[index].toPrecision(3);
+    }
+    return average;
   }
   return (
     <div>
@@ -52,7 +90,6 @@ function DashboardGraphs() {
         data={graphData}
         height={300}
         title='Temperature'/>
-      {test()}
     </div>
   );
 }
