1fef4dae44104aa946601b0d87cab03635e6712c
filip.andrei <450402@student.fontys.nl>
2021-10-12 14:27:36 +0200

    added a theme, custom _app _document files

---
+++ isaac-frontend/src/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from '@material-ui/core';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#009DDC',
+    },
+    secondary: {
+      main: '#661212',
+    },
+    fontFamily: 'Open Sans',
+  },
+});
