b021dbcec76c9827a552bc4fdae326ec9a3aca4a
filip.andrei <450402@student.fontys.nl>
2021-10-18 18:01:28 +0200

    fixed appbar fonts

--- isaac-frontend/src/components/Heatmap.js
+++ isaac-frontend/src/components/Heatmap.js
@@ -2,17 +2,28 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import {createTheme} from '@material-ui/core/styles';
 const drawerWidth = 240;
 
 const Heatmap = () => {
+  const heatmapStyle = {
+    width: `calc(100% - ${drawerWidth}px)`,
+    height: 100,
+  };
+  const fontTheme = createTheme({
+    typography: {
+      fontFamily: 'Rockwell',
+    },
+  });
   return (
     <div>
       <AppBar
         position="fixed"
         sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`}}
+        style={heatmapStyle}
       >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+        <Toolbar sx={{paddingTop: '2%'}}>
+          <Typography variant="h4" noWrap component="div" theme={fontTheme}>
             Heatmap
           </Typography>
         </Toolbar>
